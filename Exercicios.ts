5268f6d450b1eb00f0f0305e1be6d0091c3f87a8
Eduardo Gautier <duduchn2011@gmail.com>
2021-08-24 22:41:53 -0300

    Exercicios Ts

--- Exercicios.ts
+++ Exercicios.ts
@@ -60,7 +60,23 @@ function getMax(a: number[] = [1, 2, 3]) {
         }
     }
 }
+/*7. Escreva uma função que, utilizando objetos Map, calcule a frequência de cadanúmeropresenteem um determinado array contendo números inteiros.
+Utilize corretamente a declaração de tipos nos parâmetros e no resultado da função*/
+function presence(arr:number[]):Map<number,number>{
+    let result = new Map<number,number>();
 
+    arr.forEach((val:number,index:number)=>{
+        let actual = val;
+        let counter = 0;
+        arr.forEach((v:number)=>{
+            if(actual == v)counter++;
+        })
+        result.set(val,counter);
+    })
+    return result;
+}
+
+console.log(presence([2,2,2,3,4,5,6,7,8]));
 
 
 
